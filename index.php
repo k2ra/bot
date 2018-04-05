9597f1d8c991bbf667bfb4cfef1a8b08ba7f17ab
kevin <kevin_rojas30@hotmail.com>
2018-04-05 12:27:41 -0500

    prueba

--- index.php
+++ index.php
@@ -2,7 +2,31 @@
 
 
 $TOKEN = "512287569:AAFBv5fdpRAoS5eybueFs1ZGJ3e_Ytpf51Y";
-$TELEGRAM = "https://api.telegram.org/bot$TOKEN"; 
+$TELEGRAM = "https://api.telegram.org/bot".$TOKEN; 
 
-echo "funciona";
+$update = file_get_contents('php://input');
+$update = json_decode($update,TRUE);
+
+$chatId = $update["message"]["chat"]["id"];
+$chatType = $update["message"]["chat"]["type"];
+$message = $update ["message"]["text"];
+
+switch ($message) {
+	case '/search':
+		$response = "esto es el comienzo";
+		sendMessage($chatId,$response);
+		break;
+	
+	default:
+		# code...
+		break;
+}
+
+function sendMessage($chatId,$response){
+
+	$url = $GLOBAL[TELEGRAM].'/sendMessage?chat_id'.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+	file_get_contents($url);
+}
+
+//echo "funciona";
 ?>
