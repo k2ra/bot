9311b23ba5701d28393fb16d83088c5a422f2b6d
kevin <kevin_rojas30@hotmail.com>
2018-04-05 12:30:20 -0500

    prueba

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $update = json_decode($update,TRUE);
 $chatId = $update["message"]["chat"]["id"];
 $chatType = $update["message"]["chat"]["type"];
 $message = $update ["message"]["text"];
-
+echo $message;
 switch ($message) {
 	case '/search':
 		$response = "esto es el comienzo";
@@ -25,6 +25,7 @@ switch ($message) {
 function sendMessage($chatId,$response){
 
 	$url = $GLOBAL[TELEGRAM].'/sendMessage?chat_id'.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+	echo $url;
 	file_get_contents($url);
 }
 
